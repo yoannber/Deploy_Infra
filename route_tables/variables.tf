643d9e139fcb871d1ff7a01ce8739fad8844734e
Tihi <50585160+zfiel@users.noreply.github.com>
2020-03-15 18:52:23 +0100

    Update variables.tf

--- route_tables/variables.tf
+++ route_tables/variables.tf
@@ -1,47 +1,47 @@
 variable "subnet_public1_id" {
-  type = "string"
+  type = string
 }
 
 variable "subnet_public2_id" {
-  type = "string"
+  type = string
 }
 
 variable "subnet_public3_id" {
-  type = "string"
+  type = string
 }
 
 variable "subnet_private1_id" {
-  type = "string"
+  type = string
 }
 
 variable "subnet_private2_id" {
-  type = "string"
+  type = string
 }
 
 variable "subnet_private3_id" {
-  type = "string"
+  type = string
 }
 
 variable "nat_id" {
-  type = "string"
+  type = string
 }
 
 variable "default_rtb_id" {
-  type = "string"
+  type = string
 }
 
 variable "igw_id" {
-  type = "string"
+  type = string
 }
 
 variable "app_name" {
-  type = "string"
+  type = string
 }
 
 variable "env" {
-  type = "string"
+  type = string
 }
 
 variable "vpc_id" {
-  type = "string"
+  type = string
 }
