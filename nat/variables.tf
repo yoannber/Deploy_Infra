8ef359d9aec42dbaf0330b5189d5bf02b59a3892
Tihi <50585160+zfiel@users.noreply.github.com>
2020-03-15 18:51:26 +0100

    Update variables.tf

--- nat/variables.tf
+++ nat/variables.tf
@@ -1,23 +1,23 @@
 variable "aws_ami_id" {
-  type = "string"
+  type = string
 }
 
 variable "subnet_public1_id" {
-  type = "string"
+  type = string
 }
 
 variable "app_name" {
-  type = "string"
+  type = string
 }
 
 variable "env" {
-  type = "string"
+  type = string
 }
 
 variable "vpc_id" {
-  type = "string"
+  type = string
 }
 
 variable "app_cidr" {
-  type = "string"
+  type = string
 }
